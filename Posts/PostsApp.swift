22374458188d92f4c951603c9b34d27ba1f2885d
Emilly Maia <emillypereira1290@gmail.com>
2022-08-10 10:41:09 -0300

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Emilly Maia on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
